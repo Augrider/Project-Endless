d518544b2df2c3da7e39012d63dd90868c559fb3
Augrider <alotodem@gmail.com>
2026-04-06 14:24:00 +0300

    Finished Look At Pointer script

--- Player/Scripts/PlayerFollowLookPoint.gd
+++ Player/Scripts/PlayerFollowLookPoint.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var durationMultiplier:float
+
 var currentTween:Tween
 
 #@export var 
@@ -8,19 +10,29 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-func turn_to_point():
-	pass
+func _on_input_direction_look_point_changed(value: Vector2) -> void:
+	var target = get_target_angle(value)
+	rotate_towards_angle(target)
 
 
 func get_target_angle(point:Vector2)->float:
-	return get_angle_to(point)
+	return global_rotation + get_angle_to(point)
 
-func RotateTowards(target_angle:float)->void:
+func get_target_delta(point:Vector2)->float:
+	var target = get_target_angle(point)
+	var current = global_rotation
+	var delta = target-current
+	
+	if delta<=PI:
+		return delta
+	else:
+		return delta - 2*PI
+
+func rotate_towards_angle(target_angle:float)->void:
 	if currentTween != null:
 		currentTween.kill()
 		currentTween = null
 	
 	currentTween = get_tree().create_tween()
-	currentTween.tween_property(self, "rotation", rotation, target_angle)
-	currentTween.tween_property($Sprite, "scale", Vector2(), 1.0)
-	currentTween.tween_callback($Sprite.queue_free)
+	var duration = (target_angle-global_rotation)*durationMultiplier
+	currentTween.tween_property(self, "global_rotation", target_angle, duration)
